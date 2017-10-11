d5bb81aaa1c6e3f107b84ae654d65e5e80cbb6a1
espresso6615 <31956119+espresso6615@users.noreply.github.com>
2017-10-11 14:07:34 +0900

    Update day9_hw.py

--- day9_hw.py
+++ day9_hw.py
@@ -1,27 +1,25 @@
-import turtle as t
-n=5
-t.color("skyblue")
-t.speed(0)
-a=1
+import turtle as t #거북이생성
+n=5                #n을 5로 설정
+t.color("skyblue") #선색을 하늘색으로 설정
+t.speed(0)         #거북이의 속도를 최대로 설정
+a=1                #a를 1로 설정
 
-for x in range(25):
-    a=a+1
-    t.begin_fill()
-    for x in range(n):
+for x in range(25):# 25번 반복
+    a=a+1 #a에 1을 더해서 다시a에 저장
+    t.begin_fill() #채우기시작
+    for x in range(n):#n번 반복
         t.fd(75)
         t.lt(360/n)
-    t.end_fill()
-    if a%4 == 1:
+    t.end_fill()#채우기끝
+    if a%4 == 1: #a를 4로 나눈 나머지가 1일때
         t.back(75)
         t.rt(144)
-    if a%4 == 2:
+    if a%4 == 2: #a를 4로 나눈 나머지가 2일때
         t.fd(75)
         t.lt(288)    
-    if a%4 == 3:
+    if a%4 == 3: #a를 4로 나눈 나머지가 3일때
         t.back(75)
         t.rt(144)
-    else:
+    else: # 그외
         t.fd(75)
         t.lt(288)    
-    
-    
