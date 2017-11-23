daf93e2c309e94aa00c533b63887a86a327b5ac8
espresso6615 <31956119+espresso6615@users.noreply.github.com>
2017-11-23 19:07:03 +0900

    Add files via upload

---
+++ TRG.py
@@ -0,0 +1,92 @@
+import turtle as t
+import random
+
+
+#벽만들기
+t.speed(0)
+t.up()
+t.goto(-250,-250)
+t.down()
+for x in range(4):
+    t.fd(500)
+    t.lt(90)
+t.bgcolor("orange")
+#악당거북이
+te = t.Turtle()
+te.shape("turtle")
+te.color("red")
+te.speed(0)
+te.up()
+te.goto(0,200)
+#먹이
+ts = t.Turtle()
+ts.shape("circle")
+ts.color("green")
+ts.speed(0)
+ts.up()
+ts.goto(0,-200)
+#주인공거북이
+t.shape("turtle")
+t.color("white")
+t.speed(0)
+t.up()
+t.goto(0,0)
+
+
+game = False
+def turn_up():
+    t.seth(90)
+    
+def turn_right():
+    t.seth(0)
+    
+def turn_left():
+    t.seth(180)
+    
+def turn_down():
+    t.seth(270)
+    
+def play():
+    
+    t.fd(10)
+    ang = te.towards(t.pos())
+    te.seth(ang)
+    te.fd(9)
+score = 0
+def start():
+    global score
+    global game
+    if game == False:
+        game = True
+    while game:
+        play()
+       
+        g=t.distance(ts)
+        d=t.distance(te)
+        if g<10:
+            a=random.randint(-250,250)
+            b=random.randint(-250,250)
+            ts.goto(a,b)
+            print("먹었다!")
+            score+=1
+            print(score)
+        
+            
+        if d<10:
+            print("잡혔다!")
+            print("총점수:",score)
+            game= False
+
+    
+   
+t.onkeypress(turn_up,"Up")
+t.onkeypress(turn_right,"Right")
+t.onkeypress(turn_left,"Left")
+t.onkeypress(turn_down,"Down")
+t.onkeypress(start,"space")
+t.listen()
+t.mainloop()
+
+
+
+    
